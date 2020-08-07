14853ed5b9cc880fe74345bb6adea01e9eeb72be
Jimenez Jimenez <l215j162@home.ku.edu>
2020-08-07 15:47:02 -0500

    modified used of color in points and eliminated extra comments

--- Post-track-function.R
+++ Post-track-function.R
@@ -2,9 +2,11 @@
 ### This function ...
 ### ...
 #
-#' @param data_track dataframe with coordinates, categories, track class
+#' @param tracks dataframe with coordinates, categories, track class
 #' @param uncert_ras raster measuring variability
-#' @param pgly polygon (shape file)
+#' @param plyg polygon that delimits the geographical space of interest
+#' @param col.use vector of lenght two with the colors to be used in plots
+#' 
 #' @return
 #' \code{post_track} returns a dataframe with coordinates, categories,
 #' environmental values per pixel, track number, uncertainty values.
@@ -13,42 +15,37 @@
 #' environmental or geographical sampling over the uncertainty surface
 #' calculated during model selection. It allows to see whether the
 #' points occupy high or low suitability areas.
-
+#' 
 # CODE post_track ---------
-# Dependencies: 
-post_track = function(data_track, uncert_ras, pgly){
-  #selecting color ramp 
-  pal4 = colorRampPalette(c('#AF8DC3', '#7FBF7B'))
-  #mycols = brewer.pal(n = length(unique(data_track[,3])), name = 'PRGn') #for tracking points 
-  pal5 = colorRampPalette(c('lightyellow', '#FFA571')) #for uncertainty raster
-  
-  #eliminating duplicates: POTENTIALLY THIS SHOULD BE DONE IN THE OTHER FUNCTIONS... 
-  data_track$dup = paste(data_track[,3], data_track[,1], data_track[,2], sep= '_')
-  data_track = data_track[!duplicated(data_track$dup),]
-  data_track$dup = NULL
-  
-  #plotting points against uncertainty map 
-  dev.new()
-  #points 1: 
-  plot (data_track[,1], data_track[,2], 
-        main = 'Uncertainty',
-        xlab = 'Latitude',
-        ylab = 'Longitude',
-        col = pal4(length(unique(data_track[,3])))[data_track[,3]], 
-        pch = 1+data_track[,dim(data_track)[2]], cex = 0.5)
-  #raster and shape:
-  plot (uncert_ras, col = pal5(13), add = T)
-  plot (pgly, add = T)
-  #points 2: 
-  points (data_track[,1], data_track[,2], 
-          col = pal4(length(unique(data_track[,3])))[data_track[,3]], 
-          pch = 1+data_track[,dim(data_track)[2]], cex = 0.5)
-  tr.names = paste ('Transect', unique(data_track[,dim(data_track)[2]]))
-  legend ('bottomleft', legend = tr.names, pch = 1+unique(data_track[,dim(data_track)[2]]))
-  
-  #adding uncertainty values 
-  unc_data = extract (uncert_ras, data_track[,1:2])
-  def_unc = cbind (data_track, uncertainty_val = unc_data)
-  
-  return (def_unc) #definitive dataframe no longer usful for any function because ot the newer column
+# Dependencies: none
+post_track = function(tracks, uncert_ras, plyg, col.use = NULL){
+  if(is.null(col.use)){
+    print("Please define 'col.use' using two colors")
+  } else{
+    # color ramp for points
+    pal = colorRampPalette(col.use)
+    
+    # eliminating duplicates
+    tracks$dup = paste(tracks[,3], tracks[,1], tracks[,2], sep= '_')
+    tracks = tracks[!duplicated(tracks$dup),]
+    tracks$dup = NULL
+    
+    # plotting points on top of the uncertainty map 
+    dev.new()
+    # plot uncertainty raster and shapefile with region of interest
+    plot (uncert_ras, main = 'Uncertainty levels', xlab = 'Latitude', ylab = 'Longitude')
+    plot (plyg, add = T)
+    # add points 1: 
+    points(tracks[,1:2],
+          col = pal(length(unique(tracks[,3])))[tracks[,3]], 
+          pch = 1+tracks[,dim(tracks)[2]], cex = 0.5)
+    tr.names = paste('Transect', unique(tracks[,dim(tracks)[2]]))
+    legend('bottomleft', legend = tr.names, pch = 1+unique(tracks[,dim(tracks)[2]]))
+    
+    #adding uncertainty values 
+    unc_data = extract(uncert_ras, tracks[,1:2])
+    def_unc = cbind(tracks, uncertainty_val = unc_data)
+    
+    return (def_unc) 
+  }
 }
